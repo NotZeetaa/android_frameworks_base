a9bb5109c3c433a9da4fe356d8571cff522a4526
Laker <lakerman1987@gmail.com>
2021-04-13 20:43:20 +0300

    Add screenshot quality setting [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ReallySnow <reallysnow233@gmail.com>
    Change-Id: I6c0f899657568e08cdd614166b0de269a046d36f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5096,6 +5096,12 @@ public final class Settings {
         public static final String DISMISS_ALL_BUTTON = "dismiss_all_button";
 
         /**
+         * Screenshot image compression
+         * @hide
+         */
+        public static final String SCREENSHOT_COMPRESSION = "screenshot_compression";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
